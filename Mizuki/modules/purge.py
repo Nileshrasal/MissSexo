0768e70844b2bd2e6e8252882513e6dd22634bbc
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-10-10 21:15:45 +0530

    Update purge.py

--- Mizuki/modules/purge.py
+++ Mizuki/modules/purge.py
@@ -18,7 +18,7 @@ async def purge_messages(event):
 
     if not await user_is_admin(
         user_id=event.sender_id, message=event
-    ) and event.from_id not in [1087968824]:
+    ) and event.from_id not in [511112479]:
         await event.reply("Only Admins are allowed to use this command")
         return
 
@@ -56,7 +56,7 @@ async def delete_messages(event):
 
     if not await user_is_admin(
         user_id=event.sender_id, message=event
-    ) and event.from_id not in [1087968824]:
+    ) and event.from_id not in [511112479]:
         await event.reply("Only Admins are allowed to use this command")
         return
 
