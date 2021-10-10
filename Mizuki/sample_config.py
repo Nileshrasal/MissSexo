9fbf99064901c3e9f6660d85517f8f9e61676c5c
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-10-10 12:49:50 +0530

    Update sample_config.py

--- Mizuki/sample_config.py
+++ Mizuki/sample_config.py
@@ -17,14 +17,14 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sawada"
-    SUPPORT_CHAT = "OnePunchSupport"  # Your own group for support, do not add the @
+    OWNER_ID = 511112479  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "CoPYLess786"
+    SUPPORT_CHAT = "ATGKDiscuss"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+        -1001510960960
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001190806654
+        -1001510960960
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
@@ -55,7 +55,7 @@ class Config(object):
     WORKERS = (
         8  # Number of subthreads to use. Set as number of threads your processor uses
     )
-    BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
+    BAN_STICKER = "CAADBQADOgIAAktlAAFVt9t-dUT2LuIC"  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
     ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
     CASH_API_KEY = (
         "awoo"  # Get your API key from https://www.alphavantage.co/support/#api-key
